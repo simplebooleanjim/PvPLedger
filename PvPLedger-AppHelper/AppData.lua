1d969f37f83858856e61bba620a75ee3ccadd939
simplebooleanjim <222919356+simplebooleanjim@users.noreply.github.com>
2026-08-18 19:51:15 +0000

    chore(data): refresh US ladder snapshots

--- PvPLedger-AppHelper/AppData.lua
+++ PvPLedger-AppHelper/AppData.lua
@@ -3,8 +3,8 @@
 --- DO NOT EDIT MANUALLY.
 PVL_AppHelperPendingSnapshots = PVL_AppHelperPendingSnapshots or {}
 PVL_AppHelperSyncInfo = {
-    lastSync = 1787081351,
-    generatedAt = "2026-08-18T19:29:11Z",
+    lastSync = 1787082674,
+    generatedAt = "2026-08-18T19:51:14Z",
     region = "US",
     source = "collector",
 }
@@ -15,7 +15,7 @@ PVL_AppHelperPendingSnapshots.arena2v2 = {
     bracket = "arena2v2",
     season = 42,
     snapshotDate = "2026-08-18",
-    generatedAt = "2026-08-18T19:29:02Z",
+    generatedAt = "2026-08-18T19:51:05Z",
     source = "blizzard-api+seramate",
     overall = {
         listedCount = 34,
@@ -411,7 +411,7 @@ PVL_AppHelperPendingSnapshots.arena3v3 = {
     bracket = "arena3v3",
     season = 42,
     snapshotDate = "2026-08-18",
-    generatedAt = "2026-08-18T19:29:11Z",
+    generatedAt = "2026-08-18T19:51:13Z",
     source = "blizzard-api+seramate",
     overall = {
         listedCount = 6,
@@ -325085,7 +325085,7 @@ PVL_AppHelperPendingSnapshots.shuffle = {
     bracket = "shuffle",
     season = 42,
     snapshotDate = "2026-08-18",
-    generatedAt = "2026-08-18T19:28:50Z",
+    generatedAt = "2026-08-18T19:50:54Z",
     source = "blizzard-api",
     overall = {
         listedCount = 14,
