69147d19f3256761b8bdb486add4b308e6eadbc5
simplebooleanjim <222919356+simplebooleanjim@users.noreply.github.com>
2026-09-08 00:42:52 +0000

    chore(data): refresh US ladder snapshots

--- Data/LadderData_US_Rbg.lua
+++ Data/LadderData_US_Rbg.lua
@@ -2,12 +2,12 @@
 PvPLedgerLadderData = PvPLedgerLadderData or {}
 PvPLedgerLadderData.US = PvPLedgerLadderData.US or {}
 PvPLedgerLadderData.US.rbg = {
-    snapshotId = "us-rbg-s42-2026-09-07",
+    snapshotId = "us-rbg-s42-2026-09-08",
     region = "US",
     bracket = "rbg",
     season = 42,
-    snapshotDate = "2026-09-07",
-    generatedAt = "2026-09-07T22:44:30Z",
+    snapshotDate = "2026-09-08",
+    generatedAt = "2026-09-08T00:42:46Z",
     source = "blizzard-api",
     overall = {
         listedCount = 1048,
